052792a4b7847345fe8279d6f6ec6e7cf2e7a069
Pavlo555 <pavlolastdaj@gmail.com>
2025-07-07 22:26:11 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,105 @@
+// Modal const
+const openModal = document.getElementById('modalOpen');
+const closeModal = document.getElementById('modalClose');
+const modal = document.getElementById('modal');
+// Test question const
+const startTest = document.getElementById('startQuestion');
+const modalStart = document.getElementById('modalStart');
+const question = document.getElementById('question');
+const checking = document.getElementById('checking');
+const nextTest = document.getElementById('nextTest');
+
+const verbs = {
+  be: ["was/were", "been", "być"],
+  beat: ["beat", "beaten", "bić"],
+  become: ["became", "become", "stać się"],
+  begin: ["began", "begun", "rozpocząć"],
+  bet: ["bet", "bet", "zakładać się"],
+  bite: ["bit", "bitten", "gryźć"],
+  break: ["broke", "broken", "łamać"],
+  bring: ["brought", "brought", "przynosić"],
+  build: ["built", "built", "budować"],
+  burn: ["burned/burnt", "burned/burnt", "palić"],
+  buy: ["bought", "bought", "kupić"],
+  catch: ["caught", "caught", "łapać"],
+  choose: ["chose", "chosen", "wybierać"],
+  come: ["came", "come", "przyjść"],
+  cost: ["cost", "cost", "kosztować"],
+  cut: ["cut", "cut", "ciąć"],
+  do: ["did", "done", "robić"],
+  draw: ["drew", "drawn", "rysować"],
+  dream: ["dreamed/dreamt", "dreamed/dreamt", "marzyć"],
+  drink: ["drank", "drunk", "pić"],
+  drive: ["drove", "driven", "prowadzić"]
+};
+
+
+// Modal code
+openModal.onclick = function() {
+  modal.style.display = "flex";
+};
+
+modal.addEventListener('click', (e) => {
+  if (modal == e.target) {
+    modal.style.display = "none";
+  };
+});
+
+closeModal.onclick = function() {
+  modal.style.display = "none";
+};
+
+// Question code
+startTest.onclick = function() {
+  modalStart.style.display = "none";
+
+  testWord();
+};
+
+function testWord() {
+  let verbsKeys = Object.keys(verbs);
+  let keyRandom = Math.floor(Math.random() * verbsKeys.length);
+
+  let randomVerbKey = verbsKeys[keyRandom];
+  let selectedVerb = verbs[randomVerbKey];
+
+  question.innerHTML = `Zapytanie: ${randomVerbKey}`;
+
+  checking.addEventListener('click', ()=> {
+    const answerSimplePast = document.getElementById('simplePast').value.trim().toLowerCase();
+    const answerPastParticiple = document.getElementById('pastParticiple').value.trim().toLowerCase();
+    const answerTranslate = document.getElementById('translate').value.trim().toLowerCase();
+
+    if (selectedVerb[0] == answerSimplePast) {
+      document.getElementById('simplePast').value = "Prawidlowo✅";
+    } else {
+      document.getElementById('simplePast').value = `Nie Prawidlowo❌, Prawidlowo [ ${selectedVerb[0]} ]`;
+    };
+
+    if (selectedVerb[1] == answerPastParticiple) {
+      document.getElementById('pastParticiple').value = "Prawidlowo✅";
+    } else {
+      document.getElementById('pastParticiple').value = `Nie Prawidlowo❌, Prawidlowo [ ${selectedVerb[1]} ]`;
+    };
+
+    if (selectedVerb[2] == answerTranslate) {
+      document.getElementById('translate').value = "Prawidlowo✅";
+    } else {
+      document.getElementById('translate').value = `Nie Prawidlowo❌, Prawidlowo [ ${selectedVerb[2]} ]`;
+    };
+
+    checking.style.display = "none";
+    nextTest.style.display = "flex";
+  });
+};
+
+nextTest.onclick = function() {
+  document.getElementById('simplePast').value = "";
+  document.getElementById('pastParticiple').value = "";
+  document.getElementById('translate').value = "";
+
+  testWord();
+  
+  checking.style.display = "flex";
+  nextTest.style.display = "none";
+};
