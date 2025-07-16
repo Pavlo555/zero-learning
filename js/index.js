fd4a4779dade2cc97433bde552c37fa8a9f664c2
Pavlo555 <pavlolastdaj@gmail.com>
2025-07-16 17:47:51 +0200

    Вдосконалиння дизайну і логіки сайту

--- js/index.js
+++ js/index.js
@@ -11,38 +11,38 @@ const nextTest = document.getElementById('nextTest');
 let selectedVerb;
 
 const verbs = {
-  be: ["was/were", "been", "być"],
-  beat: ["beat", "beaten", "bić"],
-  become: ["became", "become", "stać się"],
-  begin: ["began", "begun", "rozpocząć"],
-  bet: ["bet", "bet", "zakładać się"],
-  bite: ["bit", "bitten", "gryźć"],
-  break: ["broke", "broken", "łamać"],
-  bring: ["brought", "brought", "przynosić"],
-  build: ["built", "built", "budować"],
-  burn: ["burned/burnt", "burned/burnt", "palić"],
-  buy: ["bought", "bought", "kupić"],
-  catch: ["caught", "caught", "łapać"],
-  choose: ["chose", "chosen", "wybierać"],
-  come: ["came", "come", "przyjść"],
-  cost: ["cost", "cost", "kosztować"],
-  cut: ["cut", "cut", "ciąć"],
-  do: ["did", "done", "robić"],
-  draw: ["drew", "drawn", "rysować"],
-  dream: ["dreamed/dreamt", "dreamed/dreamt", "marzyć"],
-  drink: ["drank", "drunk", "pić"],
-  drive: ["drove", "driven", "prowadzić"],
-  eat: ["ate", "eaten", "jeść"],
-  fall: ["fell", "fallen", "spadać"],
-  feel: ["felt", "felt", "czuć"],
-  find: ["found", "found", "znaleźć"],
-  fly: ["flew", "flown", "latać"],
-  forbid: ["forbade", "forbidden", "zakazać"],
-  forget: ["forgot", "forgotten", "zapominać"],
-  forgive: ["forgave", "forgiven", "wybaczać"],
-  get: ["got", "got/gotten", "dostawać"],
-  give: ["gave", "given", "dawać"],
-  go: ["went", "gone", "iść"]
+  be: ["was/were", "been", "być", "(бі)", "(воз / вер)", "(бін)"],
+  beat: ["beat", "beaten", "bić", "(біт)", "(біт)", "(бітен)"],
+  become: ["became", "become", "stać się", "(біком)", "(бікейм)", "(біком)"],
+  begin: ["began", "begun", "rozpocząć", "(беґін)", "(беґан)", "(беґан)"],
+  bet: ["bet", "bet", "zakładać się", "(бет)", "(бет)", "(бет)"],
+  bite: ["bit", "bitten", "gryźć", "(байт)", "(біт)", "(бітен)"],
+  break: ["broke", "broken", "łamać", "(брейк)", "(брок)", "(брокен)"],
+  bring: ["brought", "brought", "przynosić", "(брінґ)", "(брот)", "(брот)"],
+  build: ["built", "built", "budować", "(білд)", "(білт)", "(білт)"],
+  burn: ["burned/burnt", "burned/burnt", "palić", "(берн)", "(барнд / барнт)", "(барнд / барнт)"],
+  buy: ["bought", "bought", "kupić", "(бай)", "(бот)", "(бот)"],
+  catch: ["caught", "caught", "łapać", "(кетч)", "(кот)", "(кот)"],
+  choose: ["chose", "chosen", "wybierać", "(чуз)", "(чоуз)", "(чоузен)"],
+  come: ["came", "come", "przyjść", "(ком)", "(кейм)", "(ком)"],
+  cost: ["cost", "cost", "kosztować", "(кост)", "(кост)", "(кост)"],
+  cut: ["cut", "cut", "ciąć", "(кат)", "(кат)", "(кат)"],
+  do: ["did", "done", "robić", "(ду)", "(дід)", "(дан)"],
+  draw: ["drew", "drawn", "rysować", "(дро)", "(дру)", "(дрон)"],
+  dream: ["dreamed/dreamt", "dreamed/dreamt", "marzyć", "(дрім)", "(дрімд / дремт)", "(дрімд / дремт)"],
+  drink: ["drank", "drunk", "pić", "(дрінк)", "(дранк)", "(дранк)"],
+  drive: ["drove", "driven", "prowadzić", "(драйв)", "(дроув)", "(дрівен)"],
+  eat: ["ate", "eaten", "jeść", "(іт)", "(ейт)", "(ітен)"],
+  fall: ["fell", "fallen", "spadać", "(фол)", "(фел)", "(фолен)"],
+  feel: ["felt", "felt", "czuć", "(філ)", "(фелт)", "(фелт)"],
+  find: ["found", "found", "znaleźć", "(файнд)", "(фаунд)", "(фаунд)"],
+  fly: ["flew", "flown", "latać", "(флай)", "(флу)", "(фловн)"],
+  forbid: ["forbade", "forbidden", "zakazać", "(форбід)", "(форбейд)", "(форбиден)"],
+  forget: ["forgot", "forgotten", "zapominać", "(форґет)", "(форґот)", "(форґотен)"],
+  forgive: ["forgave", "forgiven", "wybaczać", "(форґів)", "(форґейв)", "(форґівен)"],
+  get: ["got", "got/gotten", "dostawać", "(ґет)", "(ґот)", "(ґот / ґотен)"],
+  give: ["gave", "given", "dawać", "(ґів)", "(ґейв)", "(ґівен)"],
+  go: ["went", "gone", "iść", "(ґоу)", "(вент)", "(ґон)"]
 };
 
 
@@ -74,19 +74,19 @@ checking.addEventListener('click', ()=> {
   const answerTranslate = document.getElementById('translate').value.trim().toLowerCase();
 
   if (selectedVerb[0] == answerSimplePast) {
-    document.getElementById('simplePast').value = "Prawidlowo✅";
+    document.getElementById('simplePast').value = `Prawidlowo✅, ${selectedVerb[0]} ${selectedVerb[4]}`;
   } else {
     document.getElementById('simplePast').value = `Nie Prawidlowo❌, Prawidlowo [ ${selectedVerb[0]} ]`;
   };
 
   if (selectedVerb[1] == answerPastParticiple) {
-    document.getElementById('pastParticiple').value = "Prawidlowo✅";
+    document.getElementById('pastParticiple').value = `Prawidlowo✅, ${selectedVerb[1]} ${selectedVerb[5]}`;
   } else {
     document.getElementById('pastParticiple').value = `Nie Prawidlowo❌, Prawidlowo [ ${selectedVerb[1]} ]`;
   };
 
   if (selectedVerb[2] == answerTranslate) {
-    document.getElementById('translate').value = "Prawidlowo✅";
+    document.getElementById('translate').value = `Prawidlowo✅, ${selectedVerb[2]}`;
   } else {
     document.getElementById('translate').value = `Nie Prawidlowo❌, Prawidlowo [ ${selectedVerb[2]} ]`;
   };
@@ -102,7 +102,7 @@ function testWord() {
   let randomVerbKey = verbsKeys[keyRandom];
   selectedVerb = verbs[randomVerbKey];
 
-  question.innerHTML = `Zapytanie: ${randomVerbKey}`;
+  question.innerHTML = `Zapytanie: ${randomVerbKey} <i style="font-size: 30px;">${selectedVerb[3]}</i>`;
 };
 
 nextTest.onclick = function() {
